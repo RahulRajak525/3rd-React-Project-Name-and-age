1a399766a346b8748b1045d8f65f9997ceee0a89
RahulRajak525 <rahulrajak525@gmail.com>
2022-11-27 01:59:43 +0530

    Wrapper and Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './Components/User/AddUser';
 import UserList from './Components/User/UserList';
@@ -10,10 +10,10 @@ function App() {
      })
   }
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UserList users={userList} />
-    </div>
+    </Fragment>
   );
 }
 
