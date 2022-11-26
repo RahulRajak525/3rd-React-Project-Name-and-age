d606dee8346359612622af3c029dd4c4a628f702
RahulRajak525 <rahulrajak525@gmail.com>
2022-11-26 21:05:00 +0530

    This is third App

---
+++ src/Components/User/UserList.js
@@ -0,0 +1,19 @@
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import Card from "../UI/Card";
+import classes from './UsersList.module.css'
+const UserList = (props) => {
+  return (
+    <Card className={classes.users} >
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+           Name: {user.name }   ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
